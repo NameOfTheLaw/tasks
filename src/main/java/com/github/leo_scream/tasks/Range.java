579ea1cf846057cefbffa9c7fd5ac9a7da802b99
Denis Verkhoturov <mod.satyr@gmail.com>
2017-07-15 12:34:00 +0300

    feat: Range implementations

--- src/main/java/com/github/leo_scream/tasks/Range.java
+++ src/main/java/com/github/leo_scream/tasks/Range.java
@@ -2,6 +2,10 @@ package com.github.leo_scream.tasks;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Specification: src/test/kotlin/com/github/leo_scream/tasks/RangeSpec.kt
@@ -13,39 +17,54 @@ public interface Range {
     int upperBound();
 
     default boolean isBefore(final Range other) {
-        throw new UnsupportedOperationException();
+        return upperBound() < other.lowerBound();
     }
 
     default boolean isAfter(final Range other) {
-        throw new UnsupportedOperationException();
+        return other.upperBound() < lowerBound();
     }
 
     default boolean isConcurrent(final Range other) {
-        throw new UnsupportedOperationException();
+        return contains(other.lowerBound()) || contains(other.upperBound());
     }
 
     default boolean contains(final int value) {
-        throw new UnsupportedOperationException();
+        return lowerBound() <= value && value <= upperBound();
     }
 
     default List<Integer> asList() {
-        throw new UnsupportedOperationException();
+        return IntStream.rangeClosed(lowerBound(), upperBound())
+                        .boxed()
+                        .collect(toList());
     }
 
     default Iterator<Integer> asIterator() {
-        throw new UnsupportedOperationException();
+        return new Iterator<Integer>() {
+            private int current = lowerBound();
+
+            @Override
+            public boolean hasNext() { return current <= upperBound(); }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                return current++;
+            }
+        };
     }
 
     static Range bounds(final int lower, final int upper) {
+        if (lower > upper)
+            throw new IllegalArgumentException("Lower bound of range must be less-than or equal to upper bound");
         return new Range() {
             @Override
             public int lowerBound() {
-                throw new UnsupportedOperationException();
+                return lower;
             }
 
             @Override
             public int upperBound() {
-                throw new UnsupportedOperationException();
+                return upper;
             }
         };
     }
