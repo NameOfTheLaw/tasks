3813a03438d89c3cbe36aa431d0c509c8c928b81
Denis Verkhoturov <mod.satyr@gmail.com>
2017-07-17 17:15:30 +0300

    feat: Traversable implementation

--- src/main/java/com/github/leo_scream/tasks/Traversable.java
+++ src/main/java/com/github/leo_scream/tasks/Traversable.java
@@ -1,6 +1,8 @@
 package com.github.leo_scream.tasks;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -12,22 +14,33 @@ public interface Traversable<T> {
     void forEach(final Consumer<T> consumer);
 
     default Traversable<T> filter(final Predicate<T> predicate) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(predicate);
+        return consumer -> this.forEach(
+                element -> { if (predicate.test(element)) consumer.accept(element); }
+        );
     }
 
     default <R> Traversable<R> map(final Function<T, R> mapper) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(mapper);
+        return consumer -> this.forEach(
+                element -> consumer.accept(mapper.apply(element))
+        );
     }
 
     default <R> Traversable<R> flatMap(final Function<T, List<R>> mapper) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(mapper);
+        return consumer -> this.forEach(
+                element -> mapper.apply(element).forEach(consumer)
+        );
     }
 
     default List<T> toList() {
-        throw new UnsupportedOperationException();
+        final List<T> list = new ArrayList<>();
+        forEach(list::add);
+        return list;
     }
 
     static <T> Traversable<T> from(final List<T> list) {
-        throw new UnsupportedOperationException();
+        return list::forEach;
     }
 }
