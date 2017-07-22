7480014fc940e9da1811fcb77cc314c61b106ba7
Denis Verkhoturov <mod.satyr@gmail.com>
2017-07-22 15:58:40 +0300

    feat: ReachIterable implementation

--- src/main/java/com/github/leo_scream/tasks/ReachIterable.java
+++ src/main/java/com/github/leo_scream/tasks/ReachIterable.java
@@ -12,34 +12,82 @@ public interface ReachIterable<T> {
     boolean forNext(Consumer<T> consumer);
 
     default ReachIterable<T> filter(final Predicate<T> predicate) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(predicate);
+        final ReachIterable<T> self = this;
+        return new ReachIterable<T>() {
+            boolean matched = false;
+            boolean hasNext = true;
+
+            @Override
+            public boolean forNext(Consumer<T> consumer) {
+                while (!matched && hasNext) {
+                    hasNext = self.forNext(
+                            element -> {
+                                matched = predicate.test(element);
+                                if (matched) consumer.accept(element);
+                            }
+                    );
+                }
+                matched = false;
+                return hasNext;
+            }
+        };
     }
 
     default <R> ReachIterable<R> map(final Function<T, R> mapper) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(mapper);
+        return consumer -> this.forNext(
+                element -> consumer.accept(mapper.apply(element))
+        );
     }
 
     default <R> ReachIterable<R> flatMap(final Function<T, List<R>> mapper) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(mapper);
+        final ReachIterable<T> outer = this;
+        return new ReachIterable<R>() {
+            ReachIterable<R> inner = ReachIterable.from(Collections.emptyList());
+            boolean outerHasNext = false;
+            boolean innerHasNext = false;
+
+            @Override
+            public boolean forNext(Consumer<R> consumer) {
+                if (!innerHasNext) {
+                    outerHasNext = outer.forNext(element -> inner = ReachIterable.from(mapper.apply(element)));
+                }
+                innerHasNext = inner.forNext(consumer);
+                return  innerHasNext || outerHasNext;
+            }
+        };
     }
 
     default boolean anyMatch(final Predicate<T> predicate) {
-        throw new UnsupportedOperationException();
+        return firstMatch(predicate).isPresent();
     }
 
     default boolean allMatch(final Predicate<T> predicate) {
-        throw new UnsupportedOperationException();
+        return !anyMatch(predicate.negate());
     }
 
     default boolean noneMatch(final Predicate<T> predicate) {
-        throw new UnsupportedOperationException();
+        return !anyMatch(predicate);
     }
 
     default Optional<T> firstMatch(final Predicate<T> predicate) {
-        throw new UnsupportedOperationException();
+        class Holder { private T value; }
+        final Holder needle = new Holder();
+        final Consumer<T> testAndExtract = element -> { if (predicate.test(element)) needle.value = element; };
+        boolean hasNext = true;
+        while (Objects.isNull(needle.value) && hasNext) {
+            hasNext = this.forNext(testAndExtract);
+        }
+        return Optional.ofNullable(needle.value);
     }
 
     static <T> ReachIterable<T> from(final List<T> list) {
-        throw new UnsupportedOperationException();
+        final Iterator<T> iterator = list.iterator();
+        return consumer -> {
+            if (iterator.hasNext()) consumer.accept(iterator.next());
+            return iterator.hasNext();
+        };
     }
 }
