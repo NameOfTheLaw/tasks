ef10517e1d44e9fd01d16ea19fb750e89af5af7b
nameofthelaw <tsibin.andr@gmail.com>
2017-08-09 23:11:00 +0300

    feat: Add ArrayList interface

---
+++ src/main/java/com/github/leo_scream/tasks/ArrayList.java
@@ -0,0 +1,26 @@
+package com.github.leo_scream.tasks;
+
+import java.util.Comparator;
+
+public interface ArrayList<V> extends List<V> {
+
+    int getCapacity();
+
+    void trimToSize();
+
+    void sort(final Comparator<V> comparator);
+
+    int binarySearch(final V val);
+
+    default void sort() {
+        throw new UnsupportedOperationException();
+    }
+
+    default void reverseSort(final Comparator<V> comparator) {
+        throw new UnsupportedOperationException();
+    }
+
+    default void reverseSort() {
+        throw new UnsupportedOperationException();
+    }
+}
