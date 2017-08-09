3717156a88fa168446ec8007e30b35d92760283c
nameofthelaw <tsibin.andr@gmail.com>
2017-08-09 23:20:18 +0300

    feat: Add LinkedList interface

---
+++ src/main/java/com/github/leo_scream/tasks/LinkedList.java
@@ -0,0 +1,28 @@
+package com.github.leo_scream.tasks;
+
+public interface LinkedList<V> extends List<V> {
+
+    V first();
+
+    V last();
+
+    default void removeFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    default void removeLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    default void push(final V val) {
+        throw new UnsupportedOperationException();
+    }
+
+    default V pop() {
+        throw new UnsupportedOperationException();
+    }
+
+    default LinkedList<V> reverse() {
+        throw new UnsupportedOperationException();
+    }
+}
