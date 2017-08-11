77f16377439a737927d36dad01fdd6e0df90dcd6
nameofthelaw <tsibin.andr@gmail.com>
2017-08-11 14:34:42 +0300

    feat: Add static initialisation method for set interface

--- src/main/java/com/github/leo_scream/tasks/collections/HashSet.java
+++ src/main/java/com/github/leo_scream/tasks/collections/HashSet.java
@@ -1,8 +1,66 @@
 package com.github.leo_scream.tasks.collections;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
 public interface HashSet<V> extends Set<V> {
 
     int getCapacity();
 
     void trimToSize();
+
+    static <V> HashSet<V> of(final V... items) {
+        final HashSet<V> set = new HashSet<V>() {
+            @Override
+            public int getCapacity() {
+                return 0;
+            }
+
+            @Override
+            public void trimToSize() {
+
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public void add(V val) {
+
+            }
+
+            @Override
+            public void remove(V val) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public boolean contains(V val) {
+                return false;
+            }
+
+            @Override
+            public void removeIf(Predicate<V> p) {
+
+            }
+
+            @Override
+            public Iterator<V> iterator() {
+                return null;
+            }
+        };
+
+        Arrays.stream(items).forEach(set::add);
+        return set;
+    }
 }
