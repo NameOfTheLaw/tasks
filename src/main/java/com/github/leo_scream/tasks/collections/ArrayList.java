c777f33248fa6d9b86deb9d516b02abb4c74a4e5
nameofthelaw <tsibin.andr@gmail.com>
2017-08-11 14:26:15 +0300

    feat: Add static initialisation methods for array and linked list

--- src/main/java/com/github/leo_scream/tasks/collections/ArrayList.java
+++ src/main/java/com/github/leo_scream/tasks/collections/ArrayList.java
@@ -1,6 +1,11 @@
 package com.github.leo_scream.tasks.collections;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.function.Predicate;
 
 public interface ArrayList<V> extends List<V> {
 
@@ -22,6 +27,89 @@ public interface ArrayList<V> extends List<V> {
 
     default ArrayList<V> reverseSort() {
         throw new UnsupportedOperationException();
+    }
+
+    static <V> ArrayList<V> of(final V... items) {
+        ArrayList<V> list = new ArrayList<V>() {
+
+            @Override
+            public Iterator<V> iterator() {
+                return null;
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public void remove(V val) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public boolean contains(V val) {
+                return false;
+            }
+
+            @Override
+            public void removeIf(Predicate<V> p) {
+
+            }
+
+            @Override
+            public void add(int i, V val) {
+
+            }
+
+            @Override
+            public void set(int i, V val) {
+
+            }
+
+            @Override
+            public V get(int i) {
+                return null;
+            }
+
+            @Override
+            public void remove(int i) {
+
+            }
+
+            @Override
+            public void subList(int from, int to) {
+
+            }
+
+            @Override
+            public int getCapacity() {
+                return 0;
+            }
+
+            @Override
+            public ArrayList<V> trimToSize() {
+                return null;
+            }
+
+            @Override
+            public ArrayList<V> sort(Comparator<V> comparator) {
+                return null;
+            }
+
+            @Override
+            public int binarySearch(V val) {
+                return 0;
+            }
+        };
+
+        Arrays.stream(items).forEach(list::add);
+        return list;
     }
 
 }
--- src/main/java/com/github/leo_scream/tasks/collections/LinkedList.java
+++ src/main/java/com/github/leo_scream/tasks/collections/LinkedList.java
@@ -1,5 +1,11 @@
 package com.github.leo_scream.tasks.collections;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
 public interface LinkedList<V> extends List<V> {
 
     V first();
@@ -24,5 +30,78 @@ public interface LinkedList<V> extends List<V> {
 
     default LinkedList<V> reverse() {
         throw new UnsupportedOperationException();
+    }
+
+    static <V> LinkedList<V> of (final V... items) {
+        LinkedList<V> list = new LinkedList<V>() {
+
+            @Override
+            public Iterator<V> iterator() {
+                return null;
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public void remove(V val) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public boolean contains(V val) {
+                return false;
+            }
+
+            @Override
+            public void removeIf(Predicate<V> p) {
+
+            }
+
+            @Override
+            public void add(int i, V val) {
+
+            }
+
+            @Override
+            public void set(int i, V val) {
+
+            }
+
+            @Override
+            public V get(int i) {
+                return null;
+            }
+
+            @Override
+            public void remove(int i) {
+
+            }
+
+            @Override
+            public void subList(int from, int to) {
+
+            }
+
+            @Override
+            public V first() {
+                return null;
+            }
+
+            @Override
+            public V last() {
+                return null;
+            }
+        };
+
+        Arrays.stream(items).forEach(list::add);
+        return list;
     }
 }
