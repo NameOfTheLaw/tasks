ac5ead8e785efcb52e5c0cc1ec9b33fb1c09f4b9
nameofthelaw <tsibin.andr@gmail.com>
2017-08-14 22:18:48 +0300

    feat: Add hash set spec

---
+++ src/test/kotlin/com/github/leo_scream/tasks/collections/HashSetSpec.kt
@@ -0,0 +1,37 @@
+package com.github.leo_scream.tasks.collections
+
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.subject.SubjectSpek
+import org.jetbrains.spek.subject.itBehavesLike
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+object HashSetSpec : SubjectSpek<HashSet<String>>({
+    subject { HashSet.of() }
+
+    itBehavesLike(SetSpec)
+
+    describe("hash set") {
+        val strings = arrayOf("a", "b", "c")
+
+        beforeEachTest { strings.forEach(subject::add) }
+
+        on("getting the capacity") {
+            val capacity = subject.capacity
+
+            it("it should be more or equals to size") {
+                assertTrue(capacity >= strings.size)
+            }
+        }
+
+        on("trimming to size") {
+            subject.trimToSize()
+
+            it("should reduce capacity to size") {
+                assertEquals(strings.size, subject.size())
+            }
+        }
+    }
+})
