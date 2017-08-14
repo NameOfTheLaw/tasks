3ef0a9d9bbaeff1e150fd4b8dfe3abd0c8e5011e
nameofthelaw <tsibin.andr@gmail.com>
2017-08-14 18:06:16 +0300

    feat: Add more specs for collection interface

--- src/test/kotlin/com/github/leo_scream/tasks/collections/CollectionSpec.kt
+++ src/test/kotlin/com/github/leo_scream/tasks/collections/CollectionSpec.kt
@@ -27,5 +27,52 @@ object CollectionSpec : SubjectSpek<Collection<String>>({
                 }
             }
         }
+
+        on("contains with the value that wasn't been added before") {
+            it("should return false") {
+                assertFalse(subject.contains("value"))
+            }
+        }
+
+        on("addition 1 element") {
+            val value = "value"
+            subject.add(value)
+
+            it("should contains added value") {
+                assertTrue(subject.contains(value))
+            }
+
+            it("should have the size of 1") {
+                assertEquals(1, subject.size())
+            }
+
+            it("should not be empty") {
+                assertTrue(subject.isEmpty)
+            }
+        }
+
+        on("addition null value") {
+            it("should throw NullPointerException") {
+                assertFailsWith<NullPointerException> { subject.add(null) }
+            }
+        }
+
+        on("removing element which is not presented in the collection") {
+            subject.remove("nonExistingValue")
+
+            it("should not change the collection size") {
+                assertEquals(1, subject.size())
+            }
+
+        }
+
+        on("removing element which is presented in the collection") {
+            subject.remove("value")
+
+            it("should reduce the collection size by 1") {
+                assertEquals(0, subject.size())
+            }
+
+        }
     }
 })
