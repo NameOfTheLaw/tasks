89593c4a1333f931547c7108adceefab789afa7f
nameofthelaw <tsibin.andr@gmail.com>
2017-08-28 22:31:36 +0300

    feat: Add traversable implementation

--- src/main/java/com/github/leo_scream/tasks/Traversable.java
+++ src/main/java/com/github/leo_scream/tasks/Traversable.java
@@ -1,6 +1,8 @@
 package com.github.leo_scream.tasks;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -8,26 +10,44 @@ import java.util.function.Predicate;
 /**
  * Specification: src/test/kotlin/com/github/leo_scream/tasks/TraversableSpec.kt
  */
+@FunctionalInterface
 public interface Traversable<T> {
     void forEach(final Consumer<T> consumer);
 
     default Traversable<T> filter(final Predicate<T> predicate) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(predicate);
+
+        return consumer -> forEach(element -> {
+            if (predicate.test(element)) {
+                consumer.accept(element);
+            }
+        });
     }
 
     default <R> Traversable<R> map(final Function<T, R> mapper) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(mapper);
+
+        return consumer -> forEach(element ->
+                consumer.accept(mapper.apply(element))
+        );
     }
 
     default <R> Traversable<R> flatMap(final Function<T, List<R>> mapper) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(mapper);
+
+        return consumer -> forEach(element ->
+                mapper.apply(element).forEach(consumer::accept)
+        );
     }
 
     default List<T> toList() {
-        throw new UnsupportedOperationException();
+        final List<T> list = new ArrayList<>();
+
+        forEach(list::add);
+        return list;
     }
 
     static <T> Traversable<T> from(final List<T> list) {
-        throw new UnsupportedOperationException();
+        return list::forEach;
     }
 }
