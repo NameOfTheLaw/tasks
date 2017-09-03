de39f1f826a91792098cde2d49d836f01f397914
nameofthelaw <tsibin.andr@gmail.com>
2017-09-03 18:01:36 +0300

    feat: Add hashset implementation

--- src/main/java/com/github/leo_scream/tasks/collections/HashSet.java
+++ src/main/java/com/github/leo_scream/tasks/collections/HashSet.java
@@ -1,6 +1,7 @@
 package com.github.leo_scream.tasks.collections;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -11,44 +12,138 @@ public interface HashSet<V> extends Set<V> {
     static <V> HashSet<V> of(final V... items) {
         final HashSet<V> set = new HashSet<V>() {
 
+            final int INITIAL_CAPACITY = 8;
+
+            final double LOAD_FACTOR = 0.75;
+
+            final double CAPACITY_MULTIPLIER = 1.5;
+            Bucket[] data = new Bucket[INITIAL_CAPACITY];
+            int size = 0;
+
             @Override
             public void forEach(Consumer<V> consumer) {
-
+                for (int i = 0; i < getCapacity(); i++)
+                    if (data[i] != null) data[i].forEach(consumer);
             }
 
             @Override
             public int size() {
-                return 0;
+                return size;
             }
 
             @Override
             public double getLoadFactor() {
-                return 0;
+                return (double) size() / getCapacity();
             }
 
             @Override
             public void add(V val) {
+                Objects.requireNonNull(val);
 
+                ensureLoadFactor();
+
+                getBucket(hash(val)).put(val);
             }
 
             @Override
             public void remove(V val) {
+                Objects.requireNonNull(val);
 
+                for (int i = 0; i < getCapacity(); i++)
+                    if (data[i] != null && data[i].remove(val)) return;
             }
 
             @Override
             public void clear() {
-
+                data = new Bucket[INITIAL_CAPACITY];
+                size = 0;
             }
 
             @Override
             public boolean contains(V val) {
+                Objects.requireNonNull(val);
+
+                for (int i = 0; i < getCapacity(); i++)
+                    if (data[i] != null && data[i].contains(val)) return true;
+
                 return false;
             }
 
             @Override
             public void removeIf(Predicate<V> p) {
+                Objects.requireNonNull(p);
 
+                for (int i = 0; i < getCapacity(); i++)
+                    if (data[i] != null) data[i].removeIf(p);
+            }
+
+            private int getCapacity() {
+                return data.length;
+            }
+
+            private int hash(V val) {
+                return hash(val, getCapacity());
+            }
+
+            private int hash(V val, int capacity) {
+                return val.hashCode() % capacity;
+            }
+
+            private void ensureLoadFactor() {
+                if (size() / getCapacity() > LOAD_FACTOR) {
+                    final int newCapacity = (int) (getCapacity() * CAPACITY_MULTIPLIER);
+                    final Bucket[] newData = new Bucket[newCapacity];
+
+                    forEach(v -> getBucket(hash(v, newCapacity), newData).put(v));
+
+                    data = newData;
+                }
+            }
+
+            private Bucket getBucket(int index) {
+                return getBucket(index, data);
+            }
+
+            private Bucket getBucket(int index, Bucket[] data) {
+                if (data[index] == null) data[index] = new Bucket();
+                return data[index];
+            }
+
+            class Bucket {
+
+
+                List<V> list = ArrayList.of();
+
+                public void put(V val) {
+                    if (!list.contains(val)) {
+                        list.add(val);
+                        size++;
+                    }
+                }
+
+                public void forEach(Consumer<V> consumer) {
+                    list.forEach(consumer);
+                }
+
+                public boolean remove(V val) {
+                    int sizeBeforeRemoving = list.size();
+                    list.remove(val);
+                    if (sizeBeforeRemoving != list.size()) {
+                        size--;
+                        return true;
+                    }
+                    return false;
+                }
+
+                public boolean contains(V val) {
+                    return list.contains(val);
+                }
+
+                public void removeIf(Predicate<V> p) {
+                    int sizeBeforeRemoving = list.size();
+                    list.removeIf(p);
+                    size -= sizeBeforeRemoving - list.size();
+                }
             }
         };
 
--- src/main/java/com/github/leo_scream/tasks/collections/Set.java
+++ src/main/java/com/github/leo_scream/tasks/collections/Set.java
@@ -3,14 +3,30 @@ package com.github.leo_scream.tasks.collections;
 public interface Set<V> extends Collection<V> {
 
     default Set<V> union(final Set<V> other) {
-        throw new UnsupportedOperationException();
+        final Set<V> unionSet = HashSet.of();
+        unionSet.addAll(this);
+        unionSet.addAll(other);
+
+        return unionSet;
     }
 
     default Set<V> intersection(final Set<V> other) {
-        throw new UnsupportedOperationException();
+        final Set<V> intersectionSet = HashSet.of();
+        intersectionSet.addAll(this);
+        intersectionSet.retainIf(other::contains);
+
+        return intersectionSet;
     }
 
     default Set<V> difference(final Set<V> other) {
-        throw new UnsupportedOperationException();
+        final Set<V> part1 = HashSet.of();
+        part1.addAll(this);
+        part1.removeIf(other::contains);
+
+        final Set<V> part2 = HashSet.of();
+        part2.addAll(other);
+        part2.removeIf(this::contains);
+
+        return part1.union(part2);
     }
 }
